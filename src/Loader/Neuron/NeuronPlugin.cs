2322bdd0af21f0c8886547f466a1e83d3a9e50a7
Valentin Arthur Thomas <warquis958@gmail.com>
2024-12-01 01:13:57 +0100

    Neuron Support

---
+++ src/Loader/Neuron/NeuronPlugin.cs
@@ -0,0 +1,45 @@
+﻿#if NR
+using Neuron.Core;
+using Neuron.Core.Plugins;
+using UnityExplorer.Config;
+
+namespace UnityExplorer.Loader.Neuron
+{
+    [Plugin(
+        Name = ExplorerCore.NAME,
+        Description = "An runtime explorer for unity",
+        Version = ExplorerCore.VERSION,
+        Author = ExplorerCore.AUTHOR,
+        Repository = "https://github.com/yukieiji/UnityExplorer/",
+        Website = "https://github.com/yukieiji/UnityExplorer/"
+    )]
+    public class NeuronPlugin : Plugin, IExplorerLoader
+    {
+        private readonly NeuronBase _neuronBase;
+        private readonly StandaloneConfigHandler _configHandler;
+
+        public string ExplorerFolderName => ExplorerCore.DEFAULT_EXPLORER_FOLDER_NAME;
+        public string ExplorerFolderDestination => _neuronBase.Platform.Configuration.BaseDirectory;
+        public string UnhollowedModulesFolder => _neuronBase.PrepareRelativeDirectory("Managed");
+
+        public ConfigHandler ConfigHandler => _configHandler;
+
+        public Action<object> OnLogMessage => Logger.Info;
+
+        public Action<object> OnLogWarning => Logger.Warn;
+
+        public Action<object> OnLogError => Logger.Error;
+
+        public NeuronPlugin()
+        {
+            _neuronBase = Globals.Get<NeuronBase>();
+            _configHandler = new StandaloneConfigHandler();
+        }
+
+        public override void Enable()
+        {
+            ExplorerCore.Init(this);
+        }
+    }
+}
+#endif
